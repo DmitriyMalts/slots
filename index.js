ef0b01fc37611351bcef2976a557cb95ba7d906d
DmitriyMalts <102922456+DmitriyMalts@users.noreply.github.com>
2022-04-09 21:30:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+function getRandomNumber() {
+    return Math.floor(Math.random() * 2) + 1;
+}
+
+function getElement(id) {
+    return document.getElementById(id);
+}
+
+function spin() {
+    const item1 = getElement('item1');
+    const item2 = getElement('item2');   
+    const item3 = getElement('item3');
+
+    const num1 = getRandomNumber();
+    const num2 = getRandomNumber();
+    const num3 = getRandomNumber();
+
+    item1.innerHTML = `${num1}`;
+    item2.innerHTML = `${num2}`;
+    item3.innerHTML = `${num3}`;
+
+    if (num1 === num2 && num1 === num3) {
+        playBoing();
+        showMessage();
+    } else {
+        hideMessage();
+    }
+}
+
+function playBoing() {
+    const boing = document.getElementById("boing");
+    boing.play();
+}
+
+function showMessage() {
+    const msg = document.getElementById("message");
+    msg.style.display = "block";
+    msg.classList.add('animated', 'pulse')
+}
+
+function hideMessage() {
+    const msg = document.getElementById("message");
+    msg.style.display = "none";
+}
